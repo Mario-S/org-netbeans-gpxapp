5b869aa6face4a959251e636fef8106a823435ed
ma.schroed <ma.schroed@netbeans.org>
2012-05-08 21:24:37 +0200

    added model for links list

--- GpxEditor/src/org/netbeans/gpx/editor/panel/MetadataPanel.java
+++ GpxEditor/src/org/netbeans/gpx/editor/panel/MetadataPanel.java
@@ -10,8 +10,13 @@
  */
 package org.netbeans.gpx.editor.panel;
 
+import com.topografix.gpx.model.Link;
 import com.topografix.gpx.model.Metadata;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.AbstractListModel;
 import javax.swing.JComponent;
+import javax.swing.ListModel;
 import org.netbeans.gpx.editor.binding.converter.XMLGregorianCalendarConverter;
 import org.netbeans.gpx.editor.GpxDataObject;
 import org.netbeans.modules.xml.multiview.ui.SectionView;
@@ -23,21 +28,26 @@ import org.netbeans.modules.xml.multiview.ui.SectionView;
 public class MetadataPanel extends AbstractMetadataPanel {
 
     private XMLGregorianCalendarConverter calendarConverter;
+    
+    private LinksListModel listModel;
 
     /** Creates new form MetadataPanel */
     public MetadataPanel(SectionView sectionView, GpxDataObject gpxDataObject) {
         super(sectionView, gpxDataObject);
 
         calendarConverter = new XMLGregorianCalendarConverter();
+
+        listModel = new LinksListModel();
+
         initComponents();
-        
+
         addModifiers();
 
         setValues();
     }
-    
-    private void addModifiers(){
-        
+
+    private void addModifiers() {
+
         addModifier(txtName);
         addModifier(txtTime);
         addModifier(txtKeywords);
@@ -51,6 +61,11 @@ public class MetadataPanel extends AbstractMetadataPanel {
             txtTime.setText(calendarConverter.convertForward(metadata.getTime()));
             txtKeywords.setText(metadata.getKeywords());
             txtAreaDesc.setText(metadata.getDesc());
+
+            List<Link> links = metadata.getLinks();
+            if (!links.isEmpty()) {
+                listModel.setLinks(links);
+            }
         }
     }
 
@@ -75,6 +90,9 @@ public class MetadataPanel extends AbstractMetadataPanel {
         lblLinks = new javax.swing.JLabel();
         scrollLinks = new javax.swing.JScrollPane();
         lstLinks = new javax.swing.JList();
+        btnAddLink = new javax.swing.JButton();
+        btnRemoveLink = new javax.swing.JButton();
+        btnEditLink = new javax.swing.JButton();
 
         lblName.setText(org.openide.util.NbBundle.getMessage(MetadataPanel.class, "MetadataPanel.lblName.text")); // NOI18N
 
@@ -90,12 +108,21 @@ public class MetadataPanel extends AbstractMetadataPanel {
 
         lblLinks.setText(org.openide.util.NbBundle.getMessage(MetadataPanel.class, "MetadataPanel.lblLinks.text")); // NOI18N
 
-        lstLinks.setModel(new javax.swing.AbstractListModel() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", " " };
-            public int getSize() { return strings.length; }
-            public Object getElementAt(int i) { return strings[i]; }
-        });
+        lstLinks.setModel(listModel);
         scrollLinks.setViewportView(lstLinks);
+
+        btnAddLink.setText(org.openide.util.NbBundle.getMessage(MetadataPanel.class, "MetadataPanel.btnAddLink.text")); // NOI18N
+
+        btnRemoveLink.setText(org.openide.util.NbBundle.getMessage(MetadataPanel.class, "MetadataPanel.btnRemoveLink.text")); // NOI18N
+        btnRemoveLink.setEnabled(false);
+
+        btnEditLink.setText(org.openide.util.NbBundle.getMessage(MetadataPanel.class, "MetadataPanel.btnEditLink.text")); // NOI18N
+        btnEditLink.setEnabled(false);
+        btnEditLink.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditLinkActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -121,8 +148,14 @@ public class MetadataPanel extends AbstractMetadataPanel {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(scrollDesc)
-                            .addComponent(scrollLinks, javax.swing.GroupLayout.DEFAULT_SIZE, 413, Short.MAX_VALUE))))
-                .addContainerGap(29, Short.MAX_VALUE))
+                            .addComponent(scrollLinks, javax.swing.GroupLayout.DEFAULT_SIZE, 413, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnAddLink)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(btnRemoveLink)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(btnEditLink)))))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -147,10 +180,23 @@ public class MetadataPanel extends AbstractMetadataPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(lblLinks)
                     .addComponent(scrollLinks, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(34, 34, 34))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnAddLink)
+                    .addComponent(btnRemoveLink)
+                    .addComponent(btnEditLink))
+                .addGap(23, 23, 23))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnEditLinkActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditLinkActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnEditLinkActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAddLink;
+    private javax.swing.JButton btnEditLink;
+    private javax.swing.JButton btnRemoveLink;
     private javax.swing.JLabel lblDesc;
     private javax.swing.JLabel lblKeywords;
     private javax.swing.JLabel lblLinks;
@@ -188,6 +234,31 @@ public class MetadataPanel extends AbstractMetadataPanel {
     public JComponent getErrorComponent(String string) {
         return null;
     }
-    
-    
+
+    private class LinksListModel extends AbstractListModel {
+
+        private List<Link> links;
+
+        public LinksListModel() {
+            links = new ArrayList<Link>();
+        }
+
+        List<Link> getLinks() {
+            return links;
+        }
+
+        void setLinks(List<Link> links) {
+            this.links = links;
+        }
+
+        @Override
+        public int getSize() {
+            return links.size();
+        }
+
+        @Override
+        public Object getElementAt(int i) {
+            return links.get(i);
+        }
+    }
 }
