c5b92314cac409f63b38f51e323c471a2fe02d4b
ma.schroed <ma.schroed@netbeans.org>
2012-09-06 15:46:32 +0200

    moved required jar for easymock

--- GpxVisual/src/org/netbeans/gpx/visual/ElevationTopComponent.java
+++ GpxVisual/src/org/netbeans/gpx/visual/ElevationTopComponent.java
@@ -55,19 +55,33 @@ public final class ElevationTopComponent extends TopComponent implements LookupL
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        elevationPanel = new javax.swing.JPanel();
+
+        javax.swing.GroupLayout elevationPanelLayout = new javax.swing.GroupLayout(elevationPanel);
+        elevationPanel.setLayout(elevationPanelLayout);
+        elevationPanelLayout.setHorizontalGroup(
+            elevationPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        elevationPanelLayout.setVerticalGroup(
+            elevationPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(elevationPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(elevationPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel elevationPanel;
     // End of variables declaration//GEN-END:variables
     @Override
     public void componentOpened() {
